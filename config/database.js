4faba17f308d4309cec484fc152f3669e5cffbee
akash-reckonsys <akash@reckonsys.com>
2025-08-28 16:39:22 +0530

    db

--- config/database.js
+++ config/database.js
@@ -2,6 +2,5 @@ module.exports = {
   // url: "mongodb://admin:Abcd123$0@ds263436.mlab.com:63436/heroku_8z9m1znp"
   //url: "mongodb://admin:Abcd123$0@ds159025.mlab.com:59025/heroku_xlbk8340"
   //  url: "mongodb://localhost:27017/"
-  url:
-    "mongodb://admin:abcd1234@cluster0-shard-00-00.cjv0p.mongodb.net:27017,cluster0-shard-00-01.cjv0p.mongodb.net:27017,cluster0-shard-00-02.cjv0p.mongodb.net:27017/numetryindia?ssl=true&replicaSet=atlas-xm4jo5-shard-0&authSource=admin&retryWrites=true&w=majority"
+  url: "mongodb+srv://akashka01_db_user:QxcCUCRMIrCEtN9g@cluster0.pysmgwz.mongodb.net/"
 };
